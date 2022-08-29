80b3c5394cf00ea16f455be88409b6373f370e6a
Ram_kumar <ramk.mahato@patancollege.edu.np>
2022-08-29 14:23:03 +0545

    initial render commit

---
+++ GyanGuru/Admin/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView, ListView, CreateView, UpdateView
+# Create your views here.
+class HomePageView(TemplateView):
+    
+    template_name = "main_pages/index.html"
+class LoginPageView(TemplateView):
+    
+    template_name = "main_pages/index.html"
